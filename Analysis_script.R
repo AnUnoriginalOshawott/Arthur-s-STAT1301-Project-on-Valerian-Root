7b72bbd23376bb0212d4b1349f2d7cda655bcee3
Charlie Boardman <c.boardman1@student.uq.edu.au>
2025-10-23 17:23:14 +1000

    Tidied  working script for data analysis

--- Analysis_script.R
+++ Analysis_script.R
@@ -1,13 +1,20 @@
-install.package(c("readxl", "dplyr", "tidir", "broom", "ggplot2"))
+#Install required packages
+install.packages(c("readxl", "dplyr", "tidyr", "broom", "ggplot2"))
+
+#Load Packages
 library(readxl)
 library(dplyr)
-library(tidir)
+library(tidyr)
 library(broom)
 library(ggplot2)
+
+#Import excel data 
 data <- read_excel("Research Project Data File.xlsx", sheet = 1, range = "G3:L64")
+
+#reformat data to make it more readable
 data_long <- data %>%
   pivot_longer(cols = matches("^[0-9]+$"), #numeric night columns 
                names_to = "Night",
-               vaules_to = "WakingMinutes") %>%
+               values_to = "WakingMinutes") %>%
   mutate(Night = as.numeric(Night))
 
